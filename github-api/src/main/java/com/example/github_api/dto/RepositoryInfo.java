9f00cbe248227ca36f268b3fcbe0325b0dc1edf5
LagsKing <jakubkrol@onet.com.pl>
2025-08-06 15:35:05 +0200

    add: github api application

---
+++ github-api/src/main/java/com/example/github_api/dto/RepositoryInfo.java
@@ -0,0 +1,27 @@
+package com.example.github_api.dto;
+
+import java.util.List;
+
+public class RepositoryInfo {
+    private String repositoryName;
+    private String ownerLogin;
+    private List<BranchInfo> branches;
+
+    public RepositoryInfo(String repositoryName, String ownerLogin, List<BranchInfo> branches) {
+        this.repositoryName = repositoryName;
+        this.ownerLogin = ownerLogin;
+        this.branches = branches;
+    }
+
+    public String getRepositoryName() {
+        return repositoryName;
+    }
+
+    public String getOwnerLogin() {
+        return ownerLogin;
+    }
+
+    public List<BranchInfo> getBranches() {
+        return branches;
+    }
+}
